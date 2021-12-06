09dfb055bd83e4c15fba467b77e15d5eff834e8b
alexwakewetail <alex@wetail.se>
2021-12-06 19:08:45 +0300

    added responsive for login and register layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,8 +7,8 @@ import Checkbox from "../../components/UI/Form/FormElements/Checkbox";
 
 const Login:FC = () => {
     return (
-        <div className='flex items-stretch justify-between'>
-            <Form classnames={['bg-white', 'w-1/2', 'mr-4' , 'rounded', 'shadow-md', 'p-4']}>
+        <div className='flex flex-wrap md:flex-nowrap items-stretch justify-between'>
+            <Form classnames={['bg-white', 'w-full', 'md:w-1/2', 'md:mr-4', 'mb-4', 'md:mb-0' , 'rounded', 'shadow-md', 'p-4']}>
                 <h2 className='mb-2 text-xl'>Login</h2>
                 <p className='text-gray-600 mb-4'>Please fill the form to login in your account</p>
                 <TextField classnames={['w-full', 'border-2', 'mb-4', 'border-gray-300', 'rounded', 'p-2']} name='username' placeholder='Enter your login/email' type='text'/>
@@ -17,7 +17,7 @@ const Login:FC = () => {
                 <Button classnames={['btn', 'btn--primary']} type='submit'>Login</Button>
             </Form>
 
-            <Form classnames={['bg-white', 'w-1/2', 'ml-4' , 'rounded', 'shadow-md', 'p-4']}>
+            <Form classnames={['bg-white', 'w-full', 'md:w-1/2', 'md:ml-4' , 'mb-4', 'md:mb-0', 'rounded', 'shadow-md', 'p-4']}>
                 <h2 className='mb-2 text-xl'>Register</h2>
                 <p className='text-gray-600 mb-4'>Don't have any accounts? Fill the form and we well register you</p>
                 <TextField classnames={['w-full', 'border-2', 'mb-4', 'border-gray-300', 'rounded', 'p-2']} name='create_user_email' placeholder='Your email' type='email'/>
